6cb2f9947aab3153169e71bc75f9617b3f891108
Karen Galstyan <karengalstyan@Karens-Air.fritz.box>
2019-02-16 00:36:42 +0100

    Added PoseNet as a React Component

---
+++ src/components/PoseNet/PoseNet.js
@@ -0,0 +1,168 @@
+import React, { Component } from "react";
+import * as posenet from "@tensorflow-models/posenet";
+import { isMobile, drawKeypoints, drawSkeleton } from '../../utils/PosNetUtils'
+
+export default class PoseNet extends Component {
+  static defaultProps = {
+    videoWidth: 600,
+    videoHeight: 480,
+    flipHorizontal: true,
+    mobileNetArchitecture: isMobile() ? 0.5 : 1.01,
+    showVideo: true,
+    showSkeleton: true,
+    showPoints: true,
+    minPoseConfidence: 0.1,
+    minPartConfidence: 0.5,
+    nmsRadius: 20.0,
+    outputStride: 16,
+    imageScaleFactor: 0.5,
+    skeletonColor: "aqua",
+    skeletonLineWidth: 2,
+    loadingText: "Please wait"
+  };
+
+  constructor(props) {
+    super(props, PoseNet.defaultProps);
+    this.state = { loading: true };
+  }
+
+  async componentWillMount() {
+    this.net = await posenet.load(this.props.mobileNetArchitecture);
+  }
+
+  async componentDidMount() {
+    try {
+      await this.setupCamera();
+    } catch (e) {
+      throw "Please enable your camera";
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
+
+  getCanvas = elem => {
+    this.canvas = elem;
+  };
+
+  getVideo = elem => {
+    this.video = elem;
+  };
+
+  async setupCamera() {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+      throw "Browser API navigator.mediaDevices.getUserMedia not available";
+    }
+
+    const { videoWidth, videoHeight } = this.props;
+    const video = this.video;
+    const mobile = isMobile();
+
+    video.width = videoWidth;
+    video.height = videoHeight;
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: {
+        facingMode: "user",
+        width: mobile ? void 0 : videoWidth,
+        height: mobile ? void 0 : videoHeight
+      }
+    });
+
+    video.srcObject = stream;
+
+    return new Promise(resolve => {
+      video.onloadedmetadata = () => {
+        // Once the video metadata is ready, we can start streaming video
+        video.play();
+        resolve(video);
+      };
+    });
+  }
+
+  poseDetectionFrame(ctx) {
+    const {
+      imageScaleFactor,
+      flipHorizontal,
+      outputStride,
+      minPoseConfidence,
+      minPartConfidence,
+      videoWidth,
+      videoHeight,
+      showVideo,
+      showPoints,
+      showSkeleton,
+      skeletonColor,
+      skeletonLineWidth
+    } = this.props;
+
+    const net = this.net;
+    const video = this.video;
+
+    const poseDetectionFrameInner = async () => {
+      let poses = [];
+
+      const pose = await net.estimateSinglePose(
+        video,
+        imageScaleFactor,
+        flipHorizontal,
+        outputStride
+      );
+
+      poses.push(pose);
+
+      ctx.clearRect(0, 0, videoWidth, videoHeight);
+      if (showVideo) {
+        ctx.save();
+        ctx.scale(-1, 1);
+        ctx.translate(-videoWidth, 0);
+        ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
+        ctx.restore();
+      }
+
+      poses.forEach(({ score, keypoints }) => {
+        if (score >= minPoseConfidence) {
+          if (showPoints) {
+            drawKeypoints(keypoints, minPartConfidence, skeletonColor, ctx);
+          }
+          if (showSkeleton) {
+            drawSkeleton(
+              keypoints,
+              minPartConfidence,
+              skeletonColor,
+              skeletonLineWidth,
+              ctx
+            );
+          }
+        }
+      });
+      requestAnimationFrame(poseDetectionFrameInner);
+    };
+    poseDetectionFrameInner();
+  }
+
+  detectPose() {
+    const { videoWidth, videoHeight } = this.props;
+    const canvas = this.canvas;
+    const ctx = canvas.getContext("2d");
+
+    canvas.width = videoWidth;
+    canvas.height = videoHeight;
+
+    this.poseDetectionFrame(ctx);
+  }
+
+  render() {
+    const loading = this.state.loading ? (
+      <div className="PoseNet__loading">{this.props.loadingText}</div>
+    ) : (
+      ""
+    );
+    return (
+      <div className="PoseNet">
+        {loading}
+        <video playsInline ref={this.getVideo} />
+        <canvas ref={this.getCanvas} />
+      </div>
+    );
+  }
+}
